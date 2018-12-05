8d37634ed48d17fb0a239af2dd95cd9eb69bb84e
muge <muge.hasilci@hotmail.com>
2018-12-05 20:59:58 +0000

    Update functionality added to the post

--- simpleblog/app/controllers/posts_controller.rb
+++ simpleblog/app/controllers/posts_controller.rb
@@ -20,9 +20,13 @@ class PostsController < ApplicationController
     end
   end
 
-  private
+    def edit
+      @post = Post.find(params[:id])
+    end
 
-  def post_params
+    private
+
+    def post_params
     params.require(:post).permit(:title, :body)
   end
 end
